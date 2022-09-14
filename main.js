96ef5e1567ddcc339bbf952d72a211cc22f96cb5
JhodanSebastianCorredorAcevedoADSO <corredorjhodan@gmail.com>
2022-09-14 09:49:23 -0500

    Ejercicio 2

--- main.js
+++ main.js
@@ -9,20 +9,48 @@ addEventListener("DOMContentLoaded", ()=>{
     console.log(`La multiplicacion es ${num1} * ${num2} = ${num1 * num2}`);
     console.log(`La division es ${num1} / ${num2} = ${num1 / num2}`);
 
-/// calcular la distancia entre dos puntos 
+/// calcular el valor por producto y total de la compra
 
-//// operacion basica **,-,+
+/// operacion basica +,*
     
-let rc=30
+let pro1=10500 
     
-let x=4
+let pro2=12500 
     
-let y=18
+let pro3=2700 
     
-let d=rc*(((x*2)-(x*1))**2+((y*2)-(y*1))**2)
+let pro4=5100  
+    
+let pro5=8000
+    
+let re1=(pro1*0.5)
+    
+let re2=(pro2*0.5)
+    
+let re3=(pro3*0.2)
+    
+let re4=(pro4*0.2)
+    
+let tot1=(pro1-re1)
+    
+let tot2=(pro2-re2)
+    
+let tot3=(pro3-re3)
+    
+let tot4=(pro4-re4)
+    
+let val=(tot1+tot2+tot3+tot4+pro5)
     
 /// resultado y operacion
+    
+console.log(`valor: ${tot1}`)
 
-console.log(`resultado:, ${d}` )
+console.log(`valor: ${tot2}`)
+
+console.log(`valor: ${tot3}`)
+
+console.log(`valor: ${tot4}`)
+
+console.log(`valor total: ${val}`)
 
 })
