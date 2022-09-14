de24b69657708c6a61a5ce2e53074f849ef7ae38
JhodanSebastianCorredorAcevedoADSO <corredorjhodan@gmail.com>
2022-09-14 10:48:41 -0500

    Ejercicio 10

--- main.js
+++ main.js
@@ -9,20 +9,12 @@ addEventListener("DOMContentLoaded", ()=>{
     console.log(`La multiplicacion es ${num1} * ${num2} = ${num1 * num2}`);
     console.log(`La division es ${num1} / ${num2} = ${num1 / num2}`);
 
-/// calcular la edad de las personas que solicitan trabajo
 
-let nombre=prompt("aqui su nombre")
+let texto, revertida
 
-let nacimi=Number(prompt("su año de nacimiento"))
+texto = "los programadores son los encargados de crear lo que las personas sueñan"
 
-let actual=2022
+revertida = texto.split("").reverse().join("");
 
-let edad = actual-nacimi
-
-/// resultdo y operacion
-
-console.log(`persona entrevistada:  ${nombre}`); 
-
-console.log(`edad de la persona: ${edad}`);
-
+console.log(revertida)
 })
