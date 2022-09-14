de3f6904c06ff1316e07c02976b7dfef880e5faf
JhodanSebastianCorredorAcevedoADSO <corredorjhodan@gmail.com>
2022-09-14 09:15:19 -0500

    Ejercicio 1

--- main.js
+++ main.js
@@ -9,10 +9,20 @@ addEventListener("DOMContentLoaded", ()=>{
     console.log(`La multiplicacion es ${num1} * ${num2} = ${num1 * num2}`);
     console.log(`La division es ${num1} / ${num2} = ${num1 / num2}`);
 
+/// calcular la distancia entre dos puntos 
 
+//// operacion basica **,-,+
+    
+let rc=30
+    
+let x=4
+    
+let y=18
+    
+let d=rc*(((x*2)-(x*1))**2+((y*2)-(y*1))**2)
+    
+/// resultado y operacion
 
-
-
-
+console.log(`resultado:, ${d}` )
 
 })
