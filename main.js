fb32921256b5626af2235f20cb73ac061c54e976
JhodanSebastianCorredorAcevedoADSO <corredorjhodan@gmail.com>
2022-09-14 09:52:57 -0500

    Ejercicio 3

--- main.js
+++ main.js
@@ -9,48 +9,30 @@ addEventListener("DOMContentLoaded", ()=>{
     console.log(`La multiplicacion es ${num1} * ${num2} = ${num1 * num2}`);
     console.log(`La division es ${num1} / ${num2} = ${num1 / num2}`);
 
-/// calcular el valor por producto y total de la compra
+/// calcular el tiempo estimado para el llenado de un deposito
 
-/// operacion basica +,*
+/// operacon basica +
     
-let pro1=10500 
+let pi=3.1416
     
-let pro2=12500 
+let rad=18
     
-let pro3=2700 
+let h=20
     
-let pro4=5100  
+let t=10
     
-let pro5=8000
+let mul=(pi*(rad**2)*h)
     
-let re1=(pro1*0.5)
+let ti=(t*60)
     
-let re2=(pro2*0.5)
-    
-let re3=(pro3*0.2)
-    
-let re4=(pro4*0.2)
-    
-let tot1=(pro1-re1)
-    
-let tot2=(pro2-re2)
-    
-let tot3=(pro3-re3)
-    
-let tot4=(pro4-re4)
-    
-let val=(tot1+tot2+tot3+tot4+pro5)
+let q=(mul/ti)
     
 /// resultado y operacion
     
-console.log(`valor: ${tot1}`)
+console.log(`volumen:, ${mul}`)
 
-console.log(`valor: ${tot2}`)
-
-console.log(`valor: ${tot3}`)
-
-console.log(`valor: ${tot4}`)
-
-console.log(`valor total: ${val}`)
+console.log(`tiempo en segundos:, ${ti}`)
+ 
+console.log(`el caudal es:, ${q}`)
 
 })
